75afb3ce36646569f961c4a3ddd45f8238050236
Simonebaca <simobaco99@gmail.com>
2023-03-24 12:52:35 +0100

    try-snack5

---
+++ snack5/main.js
@@ -0,0 +1,16 @@
+/*Scrivi una funzione che accetti tre argomenti:
+un array e due numeri ("a" più piccolo di "b" e "b" grande al massimo quanto il numero di elementi dell'array).
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra "a" e "b"*/
+
+function myFun(a, b, ...manyMoreArgs) {
+    console.log("a", a);
+    console.log("b", b);
+    console.log("manyMoreArgs", manyMoreArgs);
+  }
+  
+  myFun("one", "two", "three", "four", "five", "six");
+  
+  // Console Output:
+  // a, one
+  // b, two
+  // manyMoreArgs, ["three", "four", "five", "six"]
